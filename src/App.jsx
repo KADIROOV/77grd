533ef4f5bb3c8d3826df8d8d8e31d1c7a8cb83fd
kadiroov <akmaljonqodirov1223@gmail.com>
2025-09-10 13:51:37 +0500

    start

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black">
+      <h2 className="text-2xl text-neutral-500 font-bold w-22 mx-auto py-[10%]">
+        #77grd
+      </h2>
+    </div>
+  );
+}
